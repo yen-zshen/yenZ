5be2cefb556ba70819aa1e9dd085100eb77df6c7
yen-zshen <dianna0904@gmail.com>
2021-05-28 14:23:23 +0800

    test fb_prince

---
+++ demo/fb_test_0528/js/main.js
@@ -0,0 +1,7 @@
+
+
+let link = document.querySelector(".fbShare");
+let gameName = "公主公主公主公主公主公主公主公主公主公主公主公主";
+console.log(link)
+
+link.setAttribute("href", `https://www.facebook.com/sharer.php?u=https://weidou.com.tw/&quote=${gameName}`)
