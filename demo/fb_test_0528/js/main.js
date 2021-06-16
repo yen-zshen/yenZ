2af330ce3eb34f5d118739967914e5c916c35a7c
yen-zshen <dianna0904@gmail.com>
2021-06-16 15:40:51 +0800

    fb
    fb

--- demo/fb_test_0528/js/main.js
+++ demo/fb_test_0528/js/main.js
@@ -1,7 +1,11 @@
 
 
-let link = document.querySelector(".fbShare");
-let gameName = "公主公主公主公主公主公主公主公主公主公主公主公主";
-console.log(link)
-
-link.setAttribute("href", `https://www.facebook.com/sharer.php?u=https://weidou.com.tw/&quote=${gameName}`)
+const link = document.querySelector(".fbShare");
+const metaImgTag = document.querySelector('meta[property="og:image"]')
+let gameName = "＝＝＝可自訂內容區＝＝＝";
+let webLink = 'https://yen-zshen.github.io/yenZ/demo/fb_test_0528/index.html'
+let metaImg = "https://yen-zshen.github.io/yenZ/demo/hifuCenter/img/fb.jpg";
+console.log(link);
+console.log(metaImgTag);
+link.setAttribute("href", `https://www.facebook.com/sharer.php?u=${webLink}&quote=${gameName}`)
+// metaImgTag.setAttribute('content',metaImg);
