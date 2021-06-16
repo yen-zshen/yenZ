34f346a17817ecd508f853c29dc43e3ee6484f79
yen-zshen <dianna0904@gmail.com>
2021-06-16 18:17:42 +0800

    img
    img

--- demo/fb_test_0528/js/main.js
+++ demo/fb_test_0528/js/main.js
@@ -1,11 +1,11 @@
 
 
 const link = document.querySelector(".fbShare");
-const metaImgTag = document.querySelector('meta[property="og:image"]')
+const imgTag = document.querySelector('.imgHide');
 let gameName = "＝＝＝可自訂內容區＝＝＝";
 let webLink = 'https://yen-zshen.github.io/yenZ/demo/fb_test_0528/index.html'
-let metaImg = "https://yen-zshen.github.io/yenZ/demo/hifuCenter/img/fb.jpg";
+let imgLink = "https://yen-zshen.github.io/yenZ/demo/hifuCenter/img/fb.jpg";
 console.log(link);
-console.log(metaImgTag);
+console.log(imgTag);
 link.setAttribute("href", `https://www.facebook.com/sharer.php?u=${webLink}&quote=${gameName}`)
-metaImgTag.setAttribute('content',metaImg);
+imgTag.setAttribute('src',imgLink);
