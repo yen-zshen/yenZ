ca4831f7350a298f818176777c965e9a917f5ce4
yen-zshen <dianna0904@gmail.com>
2021-06-16 19:37:10 +0800

    v11
    v11

--- demo/fb_test_0528/js/main.js
+++ demo/fb_test_0528/js/main.js
@@ -13,77 +13,116 @@
 let webLink = 'https://yen-zshen.github.io/yenZ/demo/fb_test_0528/index.html'
 let imgLink = 'https://yen-zshen.github.io/yenZ/demo/hifuCenter/img/fb.jpg';
 
-
 window.fbAsyncInit = function() {
   FB.init({
-      appId            : '923542691836404',
-      status           : true,
-      cookie           : true,
-      version          : 'v11.0'                
+    appId      : FB_ID,
+    xfbml      : true,
+    version    : 'v2.4'
   });
-
-  $('.shareBtn').on('click',function(){
-    console.log('success click btn 3')
-    FB.ui({
-      method: 'share_open_graph',
-      action_type: 'og.shares',
-      caption: '＝＝＝可自訂 title 區 caption ＝＝＝',
-      description: '＝＝＝可自訂內容區 description ＝＝＝',
-      action_properties: JSON.stringify({
-          object : {
-             'og:url': webLink,
-             'og:title': "＝＝＝可自訂 title 區＝＝＝",
-             'og:description': '＝＝＝可自訂 內容 區＝＝＝',
-            //  'og:og:image:width': '1200',
-            //  'og:image:height': '630',
-             'og:image': imgLink
-          },
-          function(response) {}
-      })
-  });
-    // FB.ui(
-    //   {
-    //     method: 'share',
-    //     // name: 'Facebook Dialogs',
-    //     link: webLink,
-    //     picture: imgLink,
-    //     caption: '＝＝＝可自訂 title 區＝＝＝',
-    //     description: '＝＝＝可自訂內容區＝＝＝',
-    //     href: $(location).attr('href') + '?og_img=' + image,
-    //   },
-    //   function(response) {
-    //     // if (response && response.post_id) {
-    //     //   alert('Post was published.');
-    //     // } else {
-    //     //   alert('Post was not published.');
-    //     // }
-    //   }
-    // );
-  })
-  
-  // $('.shareBtn').click(function(e){
-  //   console.log('success click share 2')
-  //     e.preventDefault();
-  //     var image = imgLink;
-  //     console.log(image)
-
-  //     FB.ui(
-  //             {
-  //                 method: 'share',
-  //                 href: $(location).attr('href') + '?og_img=' + image,
-                  
-  //             },
-  //             function (response) {
-
-  //             }
-  //         );
-  // })
 };
 
 (function(d, s, id){
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+ $('.shareBtn').on('click',function(){
+  console.log('success click btn 3')
+  FB.ui({
+    method: 'share_open_graph',
+    action_type: 'og.shares',
+    caption: '＝＝＝可自訂 title 區 caption ＝＝＝',
+    description: '＝＝＝可自訂內容區 description ＝＝＝',
+    action_properties: JSON.stringify({
+        object : {
+           'og:url': webLink,
+           'og:title': "＝＝＝可自訂 title 區＝＝＝",
+           'og:description': '＝＝＝可自訂 內容 區＝＝＝',
+          //  'og:og:image:width': '1200',
+          //  'og:image:height': '630',
+           'og:image': imgLink
+        },
+        function(response) {}
+    })
+});
+ });
+
+
+
+
+// window.fbAsyncInit = function() {
+//   FB.init({
+//       appId            : '923542691836404',
+//       status           : true,
+//       cookie           : true,
+//       version          : 'v11.0'                
+//   });
+
+//   $('.shareBtn').on('click',function(){
+//     console.log('success click btn 3')
+//     FB.ui({
+//       method: 'share_open_graph',
+//       action_type: 'og.shares',
+//       caption: '＝＝＝可自訂 title 區 caption ＝＝＝',
+//       description: '＝＝＝可自訂內容區 description ＝＝＝',
+//       action_properties: JSON.stringify({
+//           object : {
+//              'og:url': webLink,
+//              'og:title': "＝＝＝可自訂 title 區＝＝＝",
+//              'og:description': '＝＝＝可自訂 內容 區＝＝＝',
+//             //  'og:og:image:width': '1200',
+//             //  'og:image:height': '630',
+//              'og:image': imgLink
+//           },
+//           function(response) {}
+//       })
+//   });
+//     // FB.ui(
+//     //   {
+//     //     method: 'share',
+//     //     // name: 'Facebook Dialogs',
+//     //     link: webLink,
+//     //     picture: imgLink,
+//     //     caption: '＝＝＝可自訂 title 區＝＝＝',
+//     //     description: '＝＝＝可自訂內容區＝＝＝',
+//     //     href: $(location).attr('href') + '?og_img=' + image,
+//     //   },
+//     //   function(response) {
+//     //     // if (response && response.post_id) {
+//     //     //   alert('Post was published.');
+//     //     // } else {
+//     //     //   alert('Post was not published.');
+//     //     // }
+//     //   }
+//     // );
+//   })
+  
+//   // $('.shareBtn').click(function(e){
+//   //   console.log('success click share 2')
+//   //     e.preventDefault();
+//   //     var image = imgLink;
+//   //     console.log(image)
+
+//   //     FB.ui(
+//   //             {
+//   //                 method: 'share',
+//   //                 href: $(location).attr('href') + '?og_img=' + image,
+                  
+//   //             },
+//   //             function (response) {
+
+//   //             }
+//   //         );
+//   // })
+// };
+
+// // (function(d, s, id){
+// //   var js, fjs = d.getElementsByTagName(s)[0];
+// //   if (d.getElementById(id)) {return;}
+// //   js = d.createElement(s); js.id = id;
+// //   js.src = "//connect.facebook.net/en_US/sdk.js";
+// //   fjs.parentNode.insertBefore(js, fjs);
+// // }(document, 'script', 'facebook-jssdk'));
