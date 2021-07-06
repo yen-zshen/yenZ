5a20945f450ee2b903dd508c2d830b8650032502
yen-zshen <dianna0904@gmail.com>
2021-07-07 02:16:17 +0800

    Update main.js

--- demo/bicycle/js/main.js
+++ demo/bicycle/js/main.js
@@ -93,7 +93,7 @@
 
 function setTest(){
 	let status = document.querySelector('.header .test');
-	status.innerHTML = `<p>版本：2</p>`
+	status.innerHTML = `<p>版本：3</p>`
 }
 setTest();
 
@@ -460,7 +460,8 @@ function show_lightBox(num){
 	$(".sec_lightBox .inner[data-type='" + num +"']").addClass("show");
 	$(".sec_lightBox").fadeIn(250);
 	tent.delay(250).fadeIn(250);
-	$('body').on('touchmove',function(el){
+	$('.wrapper').on('touchmove',function(el){
+		alert('touch move fixed')
 		el.preventDefault();
 	},false)//禁止頁面滑動
 }
