16ff088cec2889269b60be623f5b7492205c81ca
yen-zshen <dianna0904@gmail.com>
2021-07-07 15:07:38 +0800

    07

--- demo/bicycle/js/main.js
+++ demo/bicycle/js/main.js
@@ -129,7 +129,7 @@ function sendGameData() {
 		// 沒有重複
 		}else if( inputValue.idRepeat == false  ){
 			sending('game');
-			console.log('no repeat')
+			// console.log('no repeat')
 			$.ajax({
 				url: "https://script.google.com/macros/s/AKfycby1PSFHCsl48Ut2yhvG0CqzQKotDkzOO-GvU6NMYZdRt9AMxWk/exec",
 				data: {
@@ -240,6 +240,7 @@ function getGameData(){
 
 // 點選投票按鈕
 voteSubmit.addEventListener('click',function(){
+	userState.formFinish = true;
 	let ytAry = document.getElementsByName('ytVote')
 		ytAry.forEach(function(item){
 			if(item.checked){
@@ -254,9 +255,16 @@ voteSubmit.addEventListener('click',function(){
 	inputValue.address = inputFrameType.querySelector('.addressValue').value;
 	console.log(inputValue);
 	userState.fillInDataCount +=1;
-	sendVoteData();
-	fillInInputValue();
-	sending('vote');
+	veriVote();
+	if( userState.formFinish == false ){
+		alert('資料不完整')
+	}
+	else if(userState.formFinish == true){
+		sendVoteData();
+		fillInInputValue();
+		sending('vote');
+	}
+	
 	
 	
 })
@@ -304,16 +312,16 @@ function sendFinish(name){
 	})
 }
 
-// verification 驗證
+// verification 驗證 game
 function veriGame(){
 	let checkAry = [1,1,1,1,1,1,1];
 	let inputFrameType = document.querySelector('.sec_lightBox .inner[data-type="game"]');
-	let name = inputFrameType.querySelector('.nameValue').value;
-	let zwiftID = inputFrameType.querySelector('.zwiftIDValue').value;
-	let phone = inputFrameType.querySelector('.phoneValue').value;
-	let mail = inputFrameType.querySelector('.mailValue').value;
-	let age = inputFrameType.querySelector('.ageValue').value;
-	let address = inputFrameType.querySelector('.addressValue').value;
+	let name = inputFrameType.querySelector('.nameValue');
+	let zwiftID = inputFrameType.querySelector('.zwiftIDValue');
+	let phone = inputFrameType.querySelector('.phoneValue');
+	let mail = inputFrameType.querySelector('.mailValue');
+	let age = inputFrameType.querySelector('.ageValue');
+	let address = inputFrameType.querySelector('.addressValue');
 	let gameInput = [
 		{'name':'name','type':name,'notice':'* 請輸入姓名'},
 		{'name':'zwiftID','type':zwiftID,'notice':'* 請輸入 ZWIFT ID'},
@@ -323,31 +331,171 @@ function veriGame(){
 		{'name':'address','type':address,'notice':'* 請輸入寄送地址'},
 	]
 	gameInput.forEach(function(data,index){
-		if( data.type == '' ){
+		if( data.type.value == '' ){
 			checkAry[index] = 0;
 			document.querySelector(".sec_lightBox .inner[data-type='game'] .notice[data-type='"+ data.name +"']").textContent = data.notice;
+			
+			data.type.addEventListener('change',function(){
+				if(data.type.value != ''){
+					checkAry[index] = 1;
+					document.querySelector(".sec_lightBox .inner[data-type='game'] .notice[data-type='"+ data.name +"']").textContent = "";
+				}
+				if( mail.value != "" ){
+					if( !regex.test(mail.value) ){
+						checkAry[3] = 0;
+						document.querySelector(".sec_lightBox .inner[data-type='game'] .notice[data-type='mail']").textContent = "* 請填寫正確格式 sample@example.com";
+						console.log('email wrong')
+					}else if(regex.test(mail.value)){
+						checkAry[3] = 1;
+						document.querySelector(".sec_lightBox .inner[data-type='game'] .notice[data-type='mail']").textContent = " ";
+					}
+				}
+			})
 		}
 	})
+
+	//email
+	let regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	if( mail.value != "" ){
+		if( !regex.test(mail.value) ){
+			checkAry[3] = 0;
+			document.querySelector(".sec_lightBox .inner[data-type='game'] .notice[data-type='mail']").textContent = "* 請填寫正確格式";
+		}
+	}
+	
+	mail.addEventListener('change',function(){
+		if( mail.value != "" ){
+			if( !regex.test(mail.value) ){
+				checkAry[3] = 0;
+				document.querySelector(".sec_lightBox .inner[data-type='game'] .notice[data-type='mail']").textContent = "* 請填寫正確格式";
+				console.log('email wrong')
+			}else if(regex.test(mail.value)){
+				checkAry[3] = 1;
+				document.querySelector(".sec_lightBox .inner[data-type='game'] .notice[data-type='mail']").textContent = " ";
+			}
+		}
+		console.log(checkAry)
+	})
+
+	// privacy
 	let privacyAgreeGame = document.getElementById('privacyAgreeGame').checked;
 	if( privacyAgreeGame == false ){
 		checkAry[6] = 0;
 		document.querySelector(".sec_lightBox .inner[data-type='game'] .notice[data-type='privacy']").textContent = '* 請勾選';
 	}
-	console.log(checkAry)
+	document.getElementById('privacyAgreeGame').addEventListener('change',function(){
+		privacyAgreeGame = document.getElementById('privacyAgreeGame').checked;
+		if( privacyAgreeGame == true ){
+			checkAry[6] = 1;
+			document.querySelector(".sec_lightBox .inner[data-type='game'] .notice[data-type='privacy']").textContent = '';
+		}
+	})
 
+	// console.log(checkAry)
 	checkAry.forEach(function(data){
 		if( data == 0 ){
 			userState.formFinish = false;
 			document.getElementById('mCSB_1_container').style.top = 0
 			document.getElementById('mCSB_1_dragger_vertical').style.top = 0
+		}
+	})
+	
+};
+
+// verification 驗證 vote
+function veriVote(){
+	let checkAry = [1,1,1,1,1,1,1];
+	let inputFrameType = document.querySelector('.sec_lightBox .inner[data-type="vote"]');
+	let name = inputFrameType.querySelector('.nameValue');
+	let phone = inputFrameType.querySelector('.phoneValue');
+	let mail = inputFrameType.querySelector('.mailValue');
+	let age = inputFrameType.querySelector('.ageValue');
+	let address = inputFrameType.querySelector('.addressValue');
+	let ytAry = document.getElementsByName('ytVote');
+	let youtuber;
+		ytAry.forEach(function(item){
+			if(item.checked){
+				youtuber = item.value;
+			}
+		})
+	let voteInput = [
+		{'name':'name','type':name,'notice':'* 請輸入姓名'},
+		{'name':'phone','type':phone,'notice':'* 請輸入電話'},
+		{'name':'mail','type':mail,'notice':'* 請輸入 mail'},
+		{'name':'age','type':age,'notice':'* 請輸入出生年月日'},
+		{'name':'address','type':address,'notice':'* 請輸入寄送地址'},
+	]
+	voteInput.forEach(function(data,index){
+		if( data.type.value == '' ){
+			checkAry[index] = 0;
+			document.querySelector(".sec_lightBox .inner[data-type='vote'] .notice[data-type='"+ data.name +"']").textContent = data.notice;
 			
+			data.type.addEventListener('change',function(){
+				if(data.type.value != ''){
+					checkAry[index] = 1;
+					document.querySelector(".sec_lightBox .inner[data-type='vote'] .notice[data-type='"+ data.name +"']").textContent = "";
+				}
+				if( mail.value != "" ){
+					if( !regex.test(mail.value) ){
+						checkAry[3] = 0;
+						document.querySelector(".sec_lightBox .inner[data-type='vote'] .notice[data-type='mail']").textContent = "* 請填寫正確格式 sample@example.com";
+						console.log('email wrong')
+					}else if(regex.test(mail.value)){
+						checkAry[3] = 1;
+						document.querySelector(".sec_lightBox .inner[data-type='vote'] .notice[data-type='mail']").textContent = " ";
+					}
+				}
+			})
 		}
 	})
 
+	//email
+	let regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	if( mail.value != "" ){
+		if( !regex.test(mail.value) ){
+			checkAry[3] = 0;
+			document.querySelector(".sec_lightBox .inner[data-type='vote'] .notice[data-type='mail']").textContent = "* 請填寫正確格式 sample@example.com";
+		}
+	}
 	
+	mail.addEventListener('change',function(){
+		if( mail.value != "" ){
+			if( !regex.test(mail.value) ){
+				checkAry[3] = 0;
+				document.querySelector(".sec_lightBox .inner[data-type='vote'] .notice[data-type='mail']").textContent = "* 請填寫正確格式 sample@example.com";
+				console.log('email wrong')
+			}else if(regex.test(mail.value)){
+				checkAry[3] = 1;
+				document.querySelector(".sec_lightBox .inner[data-type='vote'] .notice[data-type='mail']").textContent = " ";
+			}
+		}
+		console.log(checkAry)
+	})
 
+	// privacy
+	let privacyAgreeVote = document.getElementById('privacyAgreeVote').checked;
+	if( privacyAgreeVote == false ){
+		checkAry[6] = 0;
+		document.querySelector(".sec_lightBox .inner[data-type='vote'] .notice[data-type='privacy']").textContent = '* 請勾選';
+	}
+	document.getElementById('privacyAgreeVote').addEventListener('change',function(){
+		privacyAgreeVote = document.getElementById('privacyAgreeVote').checked;
+		if( privacyAgreeVote == true ){
+			checkAry[6] = 1;
+			document.querySelector(".sec_lightBox .inner[data-type='vote'] .notice[data-type='privacy']").textContent = '';
+		}
+	})
+
+	// console.log(checkAry)
+	checkAry.forEach(function(data){
+		if( data == 0 ){
+			userState.formFinish = false;
+			document.getElementById('mCSB_1_container').style.top = 0
+			document.getElementById('mCSB_1_dragger_vertical').style.top = 0
+		}
+	})
+	
 };
-
 
 
 
