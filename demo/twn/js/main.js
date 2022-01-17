813193c082e7f51bed494c8035c2484f849bd03f
yen-zshen <dianna0904@gmail.com>
2022-01-18 02:51:29 +0800

    update 0117

--- demo/twn/js/main.js
+++ demo/twn/js/main.js
@@ -1,24 +1,15 @@
+AOS.init();
+
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
-  loop: true,
+  // loop: true,
 	autoplay: {
 		delay: 3000,
 	},
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
   },
 });
+
