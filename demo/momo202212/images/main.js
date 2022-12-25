77b1d7354584d9b1957e389f74cfaac700f0affe
starbit_yen <yen.shen@star-bit.io>
2022-12-26 04:48:42 +0800

    add momo 202212
    add momo 202212

---
+++ demo/momo202212/images/main.js
@@ -0,0 +1,15 @@
+﻿/* 回版頭*/
+jQuery(function(){
+	jQuery("#gotop").click(function(){
+		jQuery("html,body").stop(true,false).animate({scrollTop:0},700); //設定回頁面頂端
+		return false;	
+	});
+    jQuery(window).scroll(function() {
+        if ( jQuery(this).scrollTop() > 300){ //設定大於300px才顯示浮層
+            jQuery('#gotop').fadeIn("fast");
+        } else {
+            jQuery('#gotop').stop().fadeOut("fast");
+        }
+    });
+}); 
+
